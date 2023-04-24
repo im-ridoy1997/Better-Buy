b9a32e65e60f09df77d4f76df55080bc74f4e8ac
im-ridoy1997 <ayazulridoy@gmail.com>
2023-04-24 23:58:48 +0600

    edit, update and delete category functionality added

--- controllers/backendController.js
+++ controllers/backendController.js
@@ -50,10 +50,81 @@ const createCategory = (req, res) => {
             }
         });
     }
-    
+};
+
+const editCategory = (req, res, next) => {
+    const id = req.params.id;
+    connection.query(`SELECT * FROM tbl_category WHERE id = '${id}'`, (err, rows) => {
+        if(err){
+            return res.status(400).json({
+                status: false,
+                error: err.message
+            }); 
+        }else{
+            return res.status(400).json({
+                status: true,
+                data: rows[0]
+            }); 
+        }
+    });
+    next();
+};
+
+const updateCategory = (req, res, next) => {
+    const parent_id = req.body.parent_id === "" ? null : req.body.parent_id;
+    const id = req.body.id;
+    const category_name = req.body.category_name;
+    if(parent_id === null) {
+        connection.query(`UPDATE tbl_category SET category_name = '${category_name}' WHERE id = '${id}'`, (err, rows) => {
+            if(err){
+                return res.status(400).json({
+                    status: false,
+                    error: err.message
+                }); 
+            }else{
+                return res.status(200).json({
+                    status: true
+                });
+            }
+        });
+    }else{
+        connection.query(`UPDATE tbl_category SET parent_id = '${parent_id}', category_name = '${category_name}' WHERE id = '${id}'`, (err, rows) => {
+            if(err){
+                return res.status(400).json({
+                    status: false,
+                    error: err.message
+                }); 
+            }else{
+                return res.status(200).json({
+                    status: true
+                });
+            }
+        });
+    }
+    next();
+};
+
+const deleteCategory = (req, res) => {
+    const id = req.params.id;
+    connection.query(`DELETE FROM tbl_category where id = '${id}'`, (err, rows) => {
+        if(err) {
+            return res.status(400).json({
+                status: false,
+                error: err.message
+            }); 
+        }else{
+            return res.status(200).json({
+                status: true,
+                message: 'Category delete successfully.'
+            }); 
+        }
+    })
 };
 
 module.exports = {
     getCategory,
-    createCategory
+    createCategory,
+    editCategory,
+    updateCategory,
+    deleteCategory
 };
--- routes/route.js
+++ routes/route.js
@@ -7,6 +7,9 @@ const formValidation = require('../middleware/formValidation.js');
 //Admin Routes
 router.get('/admin/category', backendController.getCategory);
 router.post('/admin/category-create', formValidation.createCategory, backendController.createCategory);
+router.get('/admin/category-edit/:id', backendController.editCategory);
+router.post('/admin/category-update', formValidation.createCategory, backendController.updateCategory);
+router.delete('/admin/category/:id', backendController.deleteCategory);
 
 
 module.exports = router;
